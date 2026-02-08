88991ba34438faa09e241360899c697abc15e443
GugaValenca <gustavo_valenca@hotmail.com>
2026-02-08 11:57:51 -0600

    feat: add /api/me endpoint

--- backend/tickets/urls.py
+++ backend/tickets/urls.py
@@ -2,10 +2,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import TicketViewSet
+from .me import me
+
 
 router = DefaultRouter()
 router.register(r"tickets", TicketViewSet, basename="tickets")
 
 urlpatterns = [
+    path("me/", me,),
     path("", include(router.urls)),
 ]
