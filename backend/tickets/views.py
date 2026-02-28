11b28d73872b69aa60a8b5f3477246bafb5bb5c3
GugaValenca <gustavo_valenca@hotmail.com>
2026-02-28 12:11:49 -0600

    Optimize ticket queryset with select_related

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -1,10 +1,10 @@
+from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from django.db.models import Q
 
 from .models import Ticket
-from .serializers import TicketSerializer
 from .permissions import IsRequesterOrAssigneeOrStaff
+from .serializers import TicketSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
@@ -13,16 +13,12 @@ class TicketViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        # staff/superuser vê tudo
-        if user.is_staff or user.is_superuser:
-            return Ticket.objects.all().order_by("-created_at")
+        queryset = Ticket.objects.select_related("requester", "assignee")
 
-        # usuário comum só vê:
-        # - tickets que ele pediu (requester)
-        # - tickets atribuídos a ele (assignee)
-        return Ticket.objects.filter(
-            Q(requester=user) | Q(assignee=user)
-        ).order_by("-created_at")
+        if user.is_staff or user.is_superuser:
+            return queryset.order_by("-created_at")
+
+        return queryset.filter(Q(requester=user) | Q(assignee=user)).order_by("-created_at")
 
     def perform_create(self, serializer):
         serializer.save(requester=self.request.user)
